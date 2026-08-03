3bdfe8855ab98d3fc7b82907d429f058536b67f9
Hari Selvakumar <harisrsh2000@gmail.com>
2026-08-03 13:47:49 +0530

    Fix dark mode contrast for alternating 3x3 block colors

--- starter/static/main.js
+++ starter/static/main.js
@@ -153,6 +153,8 @@ function createBoardElement() {
       input.className = 'sudoku-cell';
       input.dataset.row = i;
       input.dataset.col = j;
+      input.dataset.blockRow = Math.floor(i / 3);
+      input.dataset.blockCol = Math.floor(j / 3);
       input.addEventListener('input', (e) => {
         const val = e.target.value.replace(/[^1-9]/g, '');
         e.target.value = val;
